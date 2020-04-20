716c85a684cb538525122a9823eedabdadff85a1
bun2works <bun2works@gmail.com>
2020-04-20 16:01:38 +0900

    add Post_model routing

---
+++ db/migrate/20200420063443_create_posts.rb
@@ -0,0 +1,9 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false, length: 100
+
+      t.timestamps
+    end
+  end
+end

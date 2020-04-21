75c5270c358383647cd59d9747c90b998497b871
bun2works <bun2works@gmail.com>
2020-04-21 11:15:14 +0900

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :teams
   resources :posts
-  root 'posts#index'
+  root 'teams#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

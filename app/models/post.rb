e12d88e40dac75866c8a96451f5c94e012a19855
bun2works <bun2works@gmail.com>
2020-04-21 15:19:18 +0900

    add Comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,7 @@ class Post < ApplicationRecord
   belongs_to :team, touch: true
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true, length: {maximum: 50}
-  validates :body, presence: true, length: {maximum: 1000}
+  validates :title, presence: true, length: {maximum: 100}
 
   scope :find_newest_post, -> (page) { order(updated_at: :desc).page(page).per(20) }
   scope :with_team_and_comment, -> { includes(team: [image_attachment: :blob], comments: [team: [image_attachment: :blob]]) }

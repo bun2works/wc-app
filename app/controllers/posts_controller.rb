5fa66321d3bff3c3967ce25d4e6e75c533c6caa8
bun2works <bun2works@gmail.com>
2020-04-21 10:15:05 +0900

    add flash validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,36 @@
 class PostsController < ApplicationController
+  before_action :authenticate_team!
+  before_action :set_post, only: [:edit, :update, :destroy]
+
+  def index
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_team.posts.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "スレッドを作成しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.with_team_and_comment.find(params[:id])
+    @comment = Comment.new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
